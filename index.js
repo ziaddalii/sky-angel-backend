443ff5142e197d7bce6f12638bb211b1c175d929
Ziad Ali <ziaddalii11@gmail.com>
2024-11-14 08:13:58 +0200

    Fix: CORS

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  methods: "GET,POST,PUT,DELETE", 
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose
