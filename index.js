2fa76be87d501ac8e1eff985821b73a58d660403
Ziad Ali <ziaddalii11@gmail.com>
2024-11-14 08:37:19 +0200

    Edit: allow from all *

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 // Allow specific origin (your frontend URL)
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin:'*', 
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
   credentials: true,
